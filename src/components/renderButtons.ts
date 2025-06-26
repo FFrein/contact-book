616b77e601349240a385db8558ca04557a415d55
unknown <pesetsky2003g@gmail.com>
2025-06-26 11:39:19 +0300

    fix: исправил проблему с адаптивом диалогового меню

--- src/components/renderButtons.ts
+++ src/components/renderButtons.ts
@@ -1,9 +1,18 @@
 export function renderButtons(){
 const TEMPLATE = `
-    <button class="btn_contacts btn_big">
+    <button id="btn_contacts_contactList" class="btn_contacts btn_big">
         <p>Добавить контакт</p>
         <img src="plus_white.svg" alt="+"/>
     </button>`;
 
     document.querySelector<HTMLDialogElement>('#app')!.insertAdjacentHTML("beforeend", TEMPLATE);
+
+    const btnContacts = document.querySelector<HTMLButtonElement>('#btn_contacts_contactList')!;
+    btnContacts.addEventListener('click', ()=>{
+        const groupsDialog = document.querySelector<HTMLDialogElement>("#contactsDialog")!;
+
+        if(groupsDialog){
+            groupsDialog.showModal();
+        }
+    });
 }
